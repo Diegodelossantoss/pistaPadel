a8c88f81b7afac2cc273872f29d0b47eb13cdd46
diegodelossantos <202104720@alu.comillas.edu>
2026-02-16 19:06:28 +0100

    init project

---
+++ src/test/java/edu/comillas/icai/gitt/pat/spring/pistaPadel/PistaPadelApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.comillas.icai.gitt.pat.spring.pistaPadel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PistaPadelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
