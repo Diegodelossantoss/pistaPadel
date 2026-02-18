ca10d3fa2de0b996292dcc828dc60a8d45904f0d
Blanca Manrique <202114643@alu.comillas.edu>
2026-02-18 15:42:12 +0100

    Registro, Login y Logout básicos

--- src/main/java/edu/comillas/icai/gitt/pat/spring/pistaPadel/controller/AuthController.java
+++ src/main/java/edu/comillas/icai/gitt/pat/spring/pistaPadel/controller/AuthController.java
@@ -30,4 +30,46 @@ public class AuthController {
         usuarioRepository.save(usuario);
         return ResponseEntity.status(HttpStatus.CREATED).body(usuario); // 201
     }
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody Usuario usuario) {
+
+        if (usuario.getEmail() == null || usuario.getPassword() == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("Falta email o password"); // 400
+        }
+
+        return usuarioRepository.findByEmail(usuario.getEmail())
+                .map(u -> {
+                    if (u.getPassword().equals(usuario.getPassword())) {
+                        return ResponseEntity.ok("Login correcto"); // 200
+                    } else {
+                        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                                .body("Credenciales incorrectas"); // 401
+                    }
+                })
+                .orElseGet(() -> ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                        .body("Credenciales incorrectas")); // 401
+    }
+    @PostMapping("/logout")
+    public ResponseEntity<?> logout() {
+        return ResponseEntity.noContent().build(); // 204
+    }
+
+
+    @GetMapping("/me")
+    public ResponseEntity<?> me(@RequestParam String email) {
+
+        if (email == null || email.isBlank()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Falta el email"); // 400
+        }
+
+        return usuarioRepository.findByEmail(email)
+                .<ResponseEntity<?>>map(ResponseEntity::ok) // 200
+                .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND)
+                        .body("Usuario no encontrado")); // 404
+    }
+
+
+
+
 }
