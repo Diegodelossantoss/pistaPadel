3efa26621b8f1657445f672fe7a5a4eb0322cf98
Lucia <luluhaller@Lucias-MacBook-Air.local>
2026-03-16 16:08:20 +0100

    cambio arreglar action

--- src/test/java/edu/comillas/icai/gitt/pat/spring/pistaPadel/controller/ReservaControllerTest.java
+++ src/test/java/edu/comillas/icai/gitt/pat/spring/pistaPadel/controller/ReservaControllerTest.java
@@ -13,6 +13,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.security.test.context.support.WithMockUser;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -23,6 +24,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -73,6 +75,7 @@ public class ReservaControllerTest {
     }
 
     @Test
+    @WithMockUser(username = "lucia@test.com", roles = "USER")
     void getReservations_shouldReturn200() throws Exception {
         mockMvc.perform(get("/pistaPadel/reservations"))
                 .andExpect(status().isOk());
